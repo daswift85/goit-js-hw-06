745663c05f8225182c641f5e21a3d4e1df1e2df4
daswift85 <114247464+daswift85@users.noreply.github.com>
2023-02-23 21:44:37 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
+const inputField = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const mainBox = document.querySelector('#boxes');
+
+
+btnCreate.addEventListener('click', () => createBoxes(inputField.value));
+btnDestroy.addEventListener('click', destroyBoxes)
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let index = 0; index < amount; index++) {
+    const newBoxItem = document.createElement('div');
+newBoxItem.style.width = `${size}px`;
+newBoxItem.style.height = `${size}px`;
+newBoxItem.style.backgroundColor = getRandomHexColor();
+mainBox.appendChild(newBoxItem);
+size += 10
+  }
+}
+
+function destroyBoxes() {
+  mainBox.innerHTML = '';
+  }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return '#' + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0');
 }
